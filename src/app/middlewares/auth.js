b7bdeeb94c4af3d2c336866a0bd2d6e2ac6edffd
andgmartins <and.gmartins@gmail.com>
2020-02-06 14:48:49 -0300

    Adjust auth.js

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -3,11 +3,7 @@ import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
-<<<<<<< HEAD
   const authHeader = req.headers.autorization || req.headers.authorization;
-=======
-  const authHeader = req.headers.autorization;
->>>>>>> 2d735f08c3004186711824e98595f39946644169
 
   if (!authHeader) {
     return res.status(401).json({ error: 'Token not provided' });
